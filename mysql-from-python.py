e0eaa6d1b98636bf69bf9ad2e26ae9d407afb149
Kevin Walton <kevywalton@gmail.com>
2019-05-24 15:26:37 +0000

    Installed pymysql to work with python & created file to run these commands

---
+++ mysql-from-python.py
@@ -0,0 +1,8 @@
+import os
+import pymysql
+
+# Get username from Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv("C9_USER")
+
+# Connect to the Database
